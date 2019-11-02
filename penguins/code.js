3638ae6e781458dfcb4a447ad4de6fe799e84bfc
Alexandra M. Boardman <alexandra.boardman@centre.edu>
2019-11-02 14:30:15 -0400

    need to work on scale

--- penguins/code.js
+++ penguins/code.js
@@ -1,4 +1,5 @@
 var penguinPromise = d3.json("classData.json");
+
 penguinPromise.then (
 function(data)
     {
@@ -6,6 +7,7 @@ function(data)
         getQuizGrade(data)
         console.log(getQuizGrade(data))
         console.log(getQuizHelper(data[0]))
+        drawGraph(data)
 
     },
 function(error) 
@@ -25,7 +27,58 @@ var getQuizGrade = function(data) {
     return x
 }
 
-var getQuizHelper = function(penguin) {
+var getQuizHelper = function(data) {
     
-    return penguin.quizes[0].grade
+    return data.quizes[0].grade
 }
+
+var ys = [7, 9, 3, 6, 7, 5, 6, 7, 5, 7, 4, 6, 5, 7, 5, 6, 9, 6, 5, 8, 5, 6, 9]  
+
+var points = ys.map(function(y,i)
+{
+    return {
+        x:i,
+        y:y 
+    }})
+    
+console.log("points", points)
+
+var screen = {width:800, height:800}
+
+var setup = function(points) {
+    d3.select("svg")
+    .attr("width", screen.width)
+    .attr("height", screen.height)
+    
+var xscale = d3.scale.linear()
+xscale.domain([0,d3.max(points, function(points) {
+    return points.x
+})])
+xscale.range([0,screen.width])
+
+var yscale=d3.scale.linear()
+y.scale.domain
+    d3.min(points, function(p) {
+        return points.y})
+    d3.max(points,function(points) {return points.y})
+    yscale.range([screen.height,0])
+    
+}
+
+
+var drawGraph = function(points, xscale, yscale) {
+    d3.select("svg")
+    .selectAll("circle")
+    .data(points)
+    .enter()
+    .append("circle")
+    .attr("r", 10)
+    .attr("cx", function(points) {
+        return xscale(points)
+    })
+    .attr("cy", function(points) {
+        return yscale(points)
+    })}
+
+
+
